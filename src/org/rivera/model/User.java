227f8d0b40b3cf5a278bcfd4e97e2e00aa7c4bdc
Stan Rivera <stan.rivera@one.verizon.com>
2022-01-21 14:16:57 -0700

    Updated the toString method

--- src/org/rivera/model/User.java
+++ src/org/rivera/model/User.java
@@ -12,17 +12,17 @@ public class User {
 		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + "]";
-	}
-
 	public int getAge() {
 		return age;
 	}
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
 	}
 	
 }
