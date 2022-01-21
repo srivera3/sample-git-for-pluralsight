746a01d9c522e122d28e1a0d42446b1b1b926750
Stan Rivera <stan.rivera@one.verizon.com>
2022-01-21 13:57:38 -0700

    Added getter and setter for the name field

--- src/org/rivera/model/User.java
+++ src/org/rivera/model/User.java
@@ -2,4 +2,13 @@ package org.rivera.model;
 
 public class User {
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
