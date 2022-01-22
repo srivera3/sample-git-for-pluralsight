5189557c41127cb571da4e78784f1b5b93c0dcfe
Stan Rivera <stan.rivera@one.verizon.com>
2022-01-21 17:28:12 -0700

    Added constructors

--- src/org/rivera/model/User.java
+++ src/org/rivera/model/User.java
@@ -3,6 +3,16 @@ package org.rivera.model;
 public class User {
 	private String name;
 	private int age;
+	
+	public User() {
+		super();
+	}
+
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
